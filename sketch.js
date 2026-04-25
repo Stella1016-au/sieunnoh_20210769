a8469078fac62f5fd22f1932d4646e2cecea9679
Sieun Noh <vhaltkfkdgo@duksung.ac.kr>
2026-04-25 21:30:43 +0900

    auto-commit

--- sketch.js
+++ sketch.js
@@ -1,27 +1,43 @@
-/** @type {import('p5')} */
-let myInput, myButton;
-let greeting = "";
+let px,py;
+let pd = 30;
+
+let dx = [100,200,300,400,500];
+let dy = [150,250,350,450,550];
+let dSize = 15;
+let dActive = [true,true,true,true,true];
+
+let score = 0;
 
 function setup(){
-  createCanvas(400,400);
-
-  myInput=createInput();
-  myInput.position(20,60);
-
-  myButton=createButton('인사하기');
-  myButton.position(myInput.x+myInput.width+10,60);
-
-  myButton.mousePressed(greetUser);
-}
-
-function greetUser(){
-  const name = myInput.value();
-  greeting = "안녕하세요,"+ name + "님";
-  myInput.value("");//입력창 비우기
+  createCanvas(600,400);
+  px = width/2;
+  py = height/2;
 }
 
 function draw(){
-  background(255);
-  textSize(24);
-  text(greeting,20,150);
+  background(30);
+
+  if(keyIsDown(LEFT_ARROW)) px-=3;
+  if(keyIsDown(RIGHT_ARROW)) px+=3;
+  if(keyIsDown(UP_ARROW)) py-=3;
+  if(keyIsDown(DOWN_ARROW)) py+=3;
+
+  fill(255,255,0);
+  ellipse(px,py,pd);//팩맨 본체
+
+  for(let i = 0; i<5; i++){
+    if(dActive[i]===true){
+      fill(255,100,100);
+      ellipse(dx[i],dy[i],dSize);
+   
+
+  let distance = dist(px,py,dx[i],dy[i]);
+
+  if(distance<(pd/2)+(dSize/2)){
+    dActive[i]=false;
+  }
+   }
+  }
+
+
 }
