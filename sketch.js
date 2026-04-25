ee0cb181e4c2a8aa6d641342638dac73ab39ea3b
Sieun Noh <vhaltkfkdgo@duksung.ac.kr>
2026-04-25 14:09:42 +0900

    auto-commit

--- sketch.js
+++ sketch.js
@@ -1,16 +1,27 @@
-let img;
+/** @type {import('p5')} */
+let myInput, myButton;
+let greeting = "";
 
-function preload() {
-  // 이미지를 미리 로딩합니다.
-  img = loadImage("Map.png"); 
+function setup(){
+  createCanvas(400,400);
+
+  myInput=createInput();
+  myInput.position(20,60);
+
+  myButton=createButton('인사하기');
+  myButton.position(myInput.x+myInput.width+10,60);
+
+  myButton.mousePressed(greetUser);
 }
 
-function setup() {
-  createCanvas(1408, 763);
-  
+function greetUser(){
+  const name = myInput.value();
+  greeting = "안녕하세요,"+ name + "님";
+  myInput.value("");//입력창 비우기
 }
 
-function draw() {
-  image(img, 0, 0,1408, 763);
-
+function draw(){
+  background(255);
+  textSize(24);
+  text(greeting,20,150);
 }
