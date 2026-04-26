3f5f92536740f682bd4cf68e03e3b618b0295b26
Sieun Noh <vhaltkfkdgo@duksung.ac.kr>
2026-04-26 19:24:12 +0900

    auto-commit

--- sketch.js
+++ sketch.js
@@ -1,39 +1,96 @@
 let img;
 let px=80;
 let py=350;//팩맨 위치
+let r = 15;//팩맨의 반지름
+
+let walls = [
+  // 외곽 테두리
+  {x: 0, y: 0, w: 180, h: 320}, {x: 160, y: 240, w: 120, h: 80},
+  {x: 0, y: 390, w: 200, h: 330}, {x: 180, y: 390, w: 100, h: 80},
+  {x: 1220, y: 0, w: 188, h: 320}, {x: 1120, y: 240, w: 120, h: 80},
+  {x: 1120, y: 390, w: 120, h: 80}, {x: 1210, y: 390, w: 300, h: 330},
+  {x: 0, y: 0, w: 1408, h: 30}, {x: 0, y: 690, w: 1408, h: 40},
+
+  // 내부 벽
+  {x: 230, y: 80, w: 80, h: 45}, {x: 365, y: 80, w: 270, h: 45},
+  {x: 770, y: 80, w: 270, h: 45}, {x: 1100, y: 80, w: 80, h: 45},
+  {x: 230, y: 175, w: 80, h: 20}, {x: 345, y: 175, w: 135, h: 20},
+  {x: 345, y: 175, w: 30, h: 90}, {x: 537, y: 170, w: 30, h: 165},
+  {x: 430, y: 240, w: 210, h: 20}, {x: 615, y: 175, w: 175, h: 20},
+  {x: 690, y: 175, w: 30, h: 85}, {x: 840, y: 170, w: 30, h: 165},
+  {x: 765, y: 243, w: 210, h: 20}, {x: 923, y: 175, w: 140, h: 20},
+  {x: 1030, y: 175, w: 35, h: 90}, {x: 1095, y: 175, w: 80, h: 20},
+  {x: 375, y: 310, w: 105, h: 90}, {x: 620, y: 310, w: 170, h: 90},
+  {x: 925, y: 310, w: 105, h: 90}, {x: 340, y: 445, w: 140, h: 20},
+  {x: 537, y: 375, w: 30, h: 95}, {x: 620, y: 445, w: 170, h: 20},
+  {x: 690, y: 445, w: 20, h: 80}, {x: 840, y: 375, w: 30, h: 95},
+  {x: 925, y: 445, w: 140, h: 20}, {x: 260, y: 510, w: 20, h: 70},
+  {x: 340, y: 510, w: 45, h: 110}, {x: 340, y: 510, w: 70, h: 20},
+  {x: 260, y: 615, w: 380, h: 20}, {x: 532, y: 560, w: 30, h: 60},
+  {x: 415, y: 565, w: 65, h: 20}, {x: 465, y: 510, w: 175, h: 20},
+  {x: 620, y: 565, w: 170, h: 20}, {x: 690, y: 565, w: 20, h: 80},
+  {x: 760, y: 510, w: 175, h: 20}, {x: 760, y: 615, w: 380, h: 15},
+  {x: 845, y: 565, w: 20, h: 60}, {x: 920, y: 565, w: 65, h: 20},
+  {x: 990, y: 510, w: 80, h: 20}, {x: 1010, y: 510, w: 60, h: 120},
+  {x: 1125, y: 510, w: 20, h: 80}, {x: 690, y: 0, w: 30, h: 125}
+];
 
 function preload() {
-  img = loadImage("Map.png"); //이미지 로드
+  //img = loadImage("Map.png"); //이미지 로드
 }
 
 function setup() {
   createCanvas(1408, 718);
-  image(img, 0, 0, 1408, 718);
-  
-  
-  
-
+  //image(img, 0, 0, 1408, 718);
 }
 
 function draw() {
-
   background(3,9,65,255);
-  drawWall();
+  drawWall();//외곽벽 그리기
+
+  let nextX = px;
+  let nextY = py;
+  
+  //팩맨 움직임 구현
+  if(keyIsDown(LEFT_ARROW)) nextX-=3;
+  if(keyIsDown(RIGHT_ARROW))nextX+=3;
+  if(keyIsDown(UP_ARROW))nextY-=3;
+  if(keyIsDown(DOWN_ARROW))nextY+=3;
+
+  //충돌하지 않으면, 계속이동한다.
+  if(!isColliding(px,py,r)){
+    px = nextX;
+    py = nextY;
+  }
+
+  fill(255,255,255,0);
+  noStroke();
+  for(let wall of walls){
+    rect(wall.x,wall.y,wall.w,wall.h);
+  }
+
+  fill(255,255,0,255);
+  arc(px,py,r*2,r*2,PI/4,PI*7/4);//팩맨
 
   if (mouseIsPressed === true) {
     let c = get(mouseX, mouseY); 
     console.log(c);
     console.log(mouseX, mouseY);
   }
+ 
+}
 
-  fill(255,255,0);
-  //arc(80,350,30,30,PI/4,PI*7/4);//팩맨
-  arc(px,py,30,30,PI/4,PI*7/4);//팩맨
-  if(keyIsDown(LEFT_ARROW))px-=3;
-  if(keyIsDown(RIGHT_ARROW))px+=3;
-  if(keyIsDown(UP_ARROW))py-=3;
-  if(keyIsDown(DOWN_ARROW))py+=3;
+function isColliding(nx,ny,r){
+  for(let wall of walls){
 
+    let closestX = constrain(nx, wall.x, wall.x + wall.w);
+    let closestY = constrain(ny, wall.y, wall.y + wall.h);
+
+    let d = dist(nx,ny,closestX,closestY);
+
+    if(d<r) return true;
+  }
+  return false;
 }
 
 function drawWall(){
